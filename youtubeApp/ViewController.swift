7be5f216310446216bd4ae6c010353edffd6f17f
GuoyiranJoy <guoyiran2001@126.com>
2022-05-12 17:17:22 +0800

    init

---
+++ youtubeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtubeApp
+//
+//  Created by ByteDance on 2022/5/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
